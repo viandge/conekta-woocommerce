db03215946e5f72974985b799ad550c89571165f
Cristian Benavides <Cristian@MacBook-Air-de-GrupoConektameSAdeCV.local>
2016-06-28 11:46:47 -0500

    Config locale from wordpress

---
+++ lang/es.php
@@ -0,0 +1,23 @@
+<?php
+	return array(
+		"monthly_installments" => array(
+			"3" => "3 meses",
+			"6" => "6 meses",
+			"9" => "9 meses",
+			"12" => "12 meses",
+		),
+		"card_expiration" => array(
+			"1" => "01 - Enero",
+			"2" => "02 - Febrero",
+			"3" => "03 - Marzo",
+			"4" => "04 - Abril",
+			"5" => "05 - Mayo",
+			"6" => "06 - Junio",
+			"7" => "07 - Julio",
+			"8" => "08 - Agosto",
+			"9" => "09 - Septiembre",
+			"10" => "10 - Octubre",
+			"11" => "11 - Noviembre",
+			"12" => "12 - Diciembre",
+		),
+	);
