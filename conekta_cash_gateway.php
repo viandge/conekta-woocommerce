3453463b7ca7d3ebfbde6ffa8f249c351cdc5aab
Andrea Santillana <andreasantillana@gmail.com>
2017-02-22 10:37:48 -0600

    remove banorte, send empty arrays

--- conekta_cash_gateway.php
+++ conekta_cash_gateway.php
@@ -201,20 +201,11 @@ class WC_Conekta_Cash_Gateway extends WC_Conekta_Plugin
             'currency'         => $data['currency'],
             'line_items'       => $line_items,
             'customer_info'    => $customer_info,
-            'shipping_contact' => $shipping_contact
+            'shipping_contact' => $shipping_contact,
+            'shipping_lines'   => $shipping_lines,
+            'discount_lines'   => $discount_lines,
+            'tax_lines'        => $tax_lines
         );
-
-        if (isset($shipping_lines)) {
-            $order_details = array_merge($order_details, array('shipping_lines' => $shipping_lines));
-        }
-
-        if (isset($discount_lines)) {
-            $order_details = array_merge($order_details, array('discount_lines' => $discount_lines));
-        }
-
-        if (isset($tax_lines)) {
-            $order_details = array_merge($order_details, array('tax_lines' => $tax_lines));
-        }
 
         if (isset($order_metadata)) {
             $order_details = array_merge($order_details, array('metadata' => $order_metadata));
